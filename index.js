7d4a78bce384da8282e9de35696b64dd745b52aa
Alber Erre <arblugo@gmail.com>
2018-11-02 20:24:33 +0000

    fox urlpath

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
     res.send('<iframe src="https://giphy.com/embed/Efpfk11XWXwTSKqgV2" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
 });
 
+app.get('/fox', function (req, res) {
+    res.send('<iframe width="560" height="315" src="https://www.youtube.com/embed/jofNR_WkoCE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+});
+
 app.listen(PORT, function () {
     console.log('Server is running, port ' + PORT);
 });
